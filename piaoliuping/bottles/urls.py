82be74b522707c0eaa53ff83e3c7a3a62dfb808d
qiu_lijie <qiu_lijie@ymail.com>
2023-08-31 04:37:54 -0700

    real new bottles page

--- piaoliuping/bottles/urls.py
+++ piaoliuping/bottles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BottlePage
+from .views import BottlePage, CreateBottlePage
 
 app_name = 'bottles'
 
 urlpatterns = [
     path('rand-bottle/', BottlePage.as_view(), name='random_bottle'),
+    path('create-bottle/', CreateBottlePage.as_view(), name='create_bottle'),
 ]
--- piaoliuping/bottles/views.py
+++ piaoliuping/bottles/views.py
@@ -1,6 +1,9 @@
 from typing import Any, Dict
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms.models import BaseModelForm
+from django.http import HttpRequest, HttpResponse
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 
 from .models import Bottle
 
@@ -12,3 +15,14 @@ class BottlePage(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context['bottle'] = Bottle.pick_random_bottle(self.request.user.pk)
         return context
+
+
+class CreateBottlePage(LoginRequiredMixin, CreateView):
+    template_name = 'bottle_form.html'
+    model = Bottle
+    fields = ['content']
+    success_url = '/'
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.user = self.request.user
+        return super().form_valid(form)
